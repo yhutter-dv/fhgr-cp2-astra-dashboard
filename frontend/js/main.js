a4f6ebd5b3f9487bb7c80a288c02526e3501501f
yhutter <hutteryannick@gmail.com>
2023-12-20 17:07:41 +0100

    Add line chart for traffic flow.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,9 +1,12 @@
 import Toastify from 'toastify-js';
+import Chart from 'chart.js/auto';
+import 'chartjs-adapter-dayjs-4/dist/chartjs-adapter-dayjs-4.esm';
 
 // Global variables
 const apiBaseUrl = "http://localhost:8000";
 const defaultMarkerColor = "#4a86cf";
 const selectedMarkerColor = "#1a5fb4";
+const updateIntervalMs = 1000;
 
 const selectedMarkerIcon = L.ExtraMarkers.icon({
     icon: 'bi-geo-fill',
@@ -29,6 +32,12 @@ let markerSelected = false;
 let selectedDirection = null;
 let selectedTimeRange = null;
 let selectedVehicleType = null;
+
+// Charts
+
+let lineChartForTrafficFlow = null;
+let lineChartForTrafficFlowUpdateInteralId = null;
+let lineChartTrafficFlowLiveUpdateEnabled = false;
 
 // Global Leaflet layers
 let stationsLayer = L.layerGroup();
@@ -132,10 +141,8 @@ async function onApplyFilterClicked(event) {
         return;
     }
     const detectorsWithIndex = getDetectorsWithIndexAccordingToFilterValues("trafficFlow");
-    console.log(selectedStation);
-    console.log("Found the following matches ", detectorsWithIndex);
     const detectorMeasurements = await getDetectorMeasurements(detectorsWithIndex);
-    console.log("Got the following measurements: ", detectorMeasurements);
+    updateLineChartForTrafficFlow(detectorMeasurements);
 }
 
 async function getDetectorMeasurements(detectorsWithIndex) {
@@ -143,7 +150,6 @@ async function getDetectorMeasurements(detectorsWithIndex) {
         "detectorMeasurements": detectorsWithIndex,
         "time": selectedTimeRange
     });
-    console.log("Sending the following body: ", body);
     const response = await fetch(`${apiBaseUrl}/detector_measurements`, {
         method: "POST",
         body: body,
@@ -200,6 +206,64 @@ function onVehicleTypeDropDownChanged(vehicleType) {
     selectedVehicleType = vehicleType;
 }
 
+
+function updateLineChartForTrafficFlow(detectorMeasurements) {
+    // Create a new series for each detector measurement
+    const dataSets = detectorMeasurements.map((detector) => {
+        return {
+            label: detector.id,
+            data: detector.measurements.map((measurement) => {
+                return {
+                    x: measurement.time,
+                    y: measurement.value
+                }
+            })
+        }
+    });
+    lineChartForTrafficFlow.data.datasets = dataSets;
+    lineChartForTrafficFlow.update();
+}
+
+function liveUpdateLineChartForTrafficFlow() {
+    console.log("Live Updating line chart for traffic flow");
+    lineChartForTrafficFlow.data.datasets.forEach(d => {
+        // TODO: Call endpoint periodically to fetch data...
+        const newData = {
+            x: new Date(),
+            y: Math.random() * 10
+        }
+        d.data.push(newData);
+    });
+    lineChartForTrafficFlow.update();
+}
+
+function createLineChartForTrafficFlow() {
+    const context = document.getElementById("line-chart-for-traffic-flow");
+    const config = {
+        type: 'line',
+        bounds: 'data',
+        options: {
+            scales: {
+                x: {
+                    type: "time",
+                }
+            },
+            responsive: true
+        },
+    };
+    const chart = new Chart(context, config);
+    return chart;
+}
+
+function toggleLiveUpdateForLineChartTrafficFlow(enabled) {
+    lineChartTrafficFlowLiveUpdateEnabled = enabled;
+    if (lineChartTrafficFlowLiveUpdateEnabled) {
+        lineChartForTrafficFlowUpdateInteralId = setInterval(liveUpdateLineChartForTrafficFlow, updateIntervalMs);
+    } else {
+        clearInterval(lineChartForTrafficFlowUpdateInteralId );
+    }
+}
+
 async function onLoad() {
     const { map, layerControl } = setupMap();
 
@@ -216,22 +280,28 @@ async function onLoad() {
     const directionDropDown = document.getElementById("direction-select");
     const timeRangeDropDown = document.getElementById("time-select");
     const vehicleTypeDropDown = document.getElementById("vehicle-select");
-
+    const checkboxLiveModeLineChartForTrafficFlow = document.getElementById("checkbox-live-mode-line-chart-traffic-flow");
     const applyFilterButton = document.getElementById("apply-filter-button");
+
+    lineChartForTrafficFlow = createLineChartForTrafficFlow();
 
     // Add Event Listeners
     cantonsDropDown.addEventListener("change", (event) => onCantonsDropDownChanged(event.target.value, map, layerControl));
     directionDropDown.addEventListener("change", (event) => onDirectionDropDownChanged(event.target.value));
     timeRangeDropDown.addEventListener("change", (event) => onTimeRangeDropDownChanged(event.target.value));
     vehicleTypeDropDown.addEventListener("change", (event) => onVehicleTypeDropDownChanged(event.target.value));
-    
+    checkboxLiveModeLineChartForTrafficFlow.addEventListener("change", (event) => toggleLiveUpdateForLineChartTrafficFlow(event.target.checked));
     applyFilterButton.addEventListener("click", onApplyFilterClicked);
+
+    checkboxLiveModeLineChartForTrafficFlow.checked = lineChartTrafficFlowLiveUpdateEnabled;
+    toggleLiveUpdateForLineChartTrafficFlow(checkboxLiveModeLineChartForTrafficFlow.checked);
 
     // Trigger inital change manually so the map gets updated properly 
     onCantonsDropDownChanged(cantonsDropDown.options[cantonsDropDown.selectedIndex].value, map, layerControl);
     onDirectionDropDownChanged(directionDropDown.options[directionDropDown.selectedIndex].value);
     onTimeRangeDropDownChanged(timeRangeDropDown.options[timeRangeDropDown.selectedIndex].value);
     onVehicleTypeDropDownChanged(vehicleTypeDropDown.options[vehicleTypeDropDown.selectedIndex].value);
+
 }
 
 window.addEventListener("load", onLoad)
